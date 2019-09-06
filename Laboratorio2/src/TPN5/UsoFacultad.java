524cb2a4ae61296bb0c409b63c214edb9b0f8fb4
adrianvillalba1993 <53920195+adrianvillalba1993@users.noreply.github.com>
2019-09-06 15:27:27 -0300

    Update UsoFacultad.java

--- Laboratorio2/src/TPN5/UsoFacultad.java
+++ Laboratorio2/src/TPN5/UsoFacultad.java
@@ -3,6 +3,11 @@ package TPN5;
 /**
  * Created by adrian on 29/08/2019.
  */
+/*Grupo: Silla verde
+
+NOTA: la clase "Menu" es un agregado extra mio , no para entregar junto al trabajo sino como un manera  propia para testear cada
+metodo de las distintas clases.
+*/
 public class UsoFacultad {
     public static void main(String[]args){
         Menu menu = new Menu();
